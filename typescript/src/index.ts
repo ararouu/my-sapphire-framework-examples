d8e0721327f5c6eb9fcedb1daad7c315243f96f5
emkie8241 <103377462+emkie8241@users.noreply.github.com>
2022-09-10 19:49:32 -0500

    Add files via upload

---
+++ typescript/src/index.ts
@@ -0,0 +1,16 @@
+import { SapphireClient, container, LogLevel } from "@sapphire/framework";
+import { config } from "./config.js";
+
+const client = new Client({
+  intents: 32767, //All intents.
+  partials: [''], //Add partials if needed.
+  defaultPrefix: config.bot.prefix,
+  loadMessageCommandListeners: true, //Loads commands usable with prefixes.
+  caseInsensitiveCommands: true, //Allows members to use commands with command name or alias being case insensitive.
+  logger: {
+   level: LogLevel.Debug //Allows logging of debug and higher.
+    }
+  });
+
+
+client.login(config.bot.token);
